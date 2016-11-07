d22b731c69a0bcc0adc8031f69407a2aaa503cc6
TJ Walker <tjwalker@live.ca>
2016-11-07 17:34:07 -0500

    log in page trail

--- javascripts/main.js
+++ javascripts/main.js
@@ -4,3 +4,10 @@ nb.addEventListener('click', function() {
     document.getElementById("state-initial").style.display = "none";
     document.getElementById("state-time").style.display = "flex";
 }, false);
+
+var logIn = document.getElementById('state-log-in');
+
+logIn.addEventListener('click', function() {
+    document.getElementById("state-initial").style.display = "none";
+    document.getElementById("state-log-in").style.display = "flex";
+}, false);
