fb259820aee8cfb0e830383f8cef2c3bd5121e69
Gabe <dalimogj@mcmaster.ca>
2016-11-07 17:56:37 -0500

    JS and stuff for Time state.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,14 @@
+var payment_time = 0;
+var max_time = 120;
+var payment_interval = 30;
+
 document.getElementById("state-initial-new-button").addEventListener('click', function() {
     document.getElementById("state-initial").style.display = "none";
+
+    payment_time = 0;
+    document.getElementById("state-time-exp-time").innerText = payment_time+" minutes.";
+    document.getElementById("state-time-minus-time").disabled = true;
+    document.getElementById("state-time-pay-button").disabled = true;
     document.getElementById("state-time").style.display = "flex";
 }, false);
 
@@ -13,3 +22,24 @@ document.getElementById("state-time-cancel-button").addEventListener('click', fu
     document.getElementById("state-initial").style.display = "flex";
     document.getElementById("state-time").style.display = "none";
 }, false);
+
+document.getElementById("state-time-plus-time").addEventListener('click', function() {
+	if ((payment_time+payment_interval) <= max_time){
+	    payment_time += payment_interval;
+	    document.getElementById("state-time-exp-time").innerText = payment_time+" minutes.";
+	    document.getElementById("state-time-minus-time").disabled = false;
+	    document.getElementById("state-time-pay-button").disabled = false;
+	}
+}, false);
+
+document.getElementById("state-time-minus-time").addEventListener('click', function() {
+	if((payment_time-payment_interval) >= 0) {
+	    payment_time -= payment_interval;
+	    document.getElementById("state-time-exp-time").innerText = payment_time+" minutes.";
+	    if(payment_time == 0) {
+		    document.getElementById("state-time-minus-time").disabled = true;
+		    document.getElementById("state-time-pay-button").disabled = true;
+		}
+	}
+}, false);
+
